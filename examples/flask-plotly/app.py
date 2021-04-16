dde1bbc85006b22e4610e47b6fd6b2d9b3ae582a
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2021-04-15 20:29:56 -0400

    Add Flask and Plotly example

---
+++ examples/flask-plotly/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/plot', methods=['POST'])
+def plot():
+    xdata = request.form['xdata']
+    ydata = request.form['ydata']
+
+    x = list(map(float, xdata.split(', ')))
+    y = list(map(float, ydata.split(', ')))
+    data = {'x': x, 'y': y}
+
+    return render_template('plot.html', data=data)
