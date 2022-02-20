1cf53b5bebb14536b91b2c981c8225877560faea
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2022-02-20 12:53:12 -0500

    Add a Flask example Bokeh plot

--- examples/flask-bokeh/app.py
+++ examples/flask-bokeh/app.py
@@ -16,14 +16,19 @@ def index():
 
 @app.route('/plot', methods=['POST'])
 def plot():
+    # Get the form inputs
     xdata = request.form['xdata']
     ydata = request.form['ydata']
 
+    # Convert form input to a list of floats representing x and y values
     x = list(map(float, xdata.split(', ')))
     y = list(map(float, ydata.split(', ')))
 
+    # Create a Bokeh plot using the x and y values
     p = figure(plot_width=400, plot_height=400)
     p.circle(x, y, size=12, line_color='navy', fill_color='orange')
+
+    # Get HTML components to embed in a web page
     script, div = components(p)
 
     return render_template('plot.html', script=script, div=div)
