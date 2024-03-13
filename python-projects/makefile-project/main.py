33038be6840dfecfd8d1dc22de8f8431f43fd83c
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-03-12 22:55:27 -0400

    Add a makefile example

---
+++ python-projects/makefile-project/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def adder(x, y):
+    """
+    Add two numbers.
+    """
+    a = x + y
+    return a
+
+def main():
+    """
+    here
+    """
+    s = adder(1, 3)
+    print("sum is", s)
+
+if __name__ == '__main__':
+    main()
---
+++ python-projects/makefile-project/test_adder.py
@@ -0,0 +1,5 @@
+from main import adder
+
+def test_adder():
+    s = adder(1, 3)
+    assert s == 5
